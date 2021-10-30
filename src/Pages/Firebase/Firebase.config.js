60a0edad46d98f0ecc27e84b638de20afe920b1c
Kaushik-Pramanik <pramanikk33@gmail.com>
2021-10-30 13:55:43 +0600

    firebase problem solved

--- src/Pages/Firebase/Firebase.config.js
+++ src/Pages/Firebase/Firebase.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB_29BaBmZvNTbTos-Ce78P0Zv0G6G3Idk",
-    authDomain: "first-project-da3e4.firebaseapp.com",
-    projectId: "first-project-da3e4",
-    storageBucket: "first-project-da3e4.appspot.com",
-    messagingSenderId: "498644246145",
-    appId: "1:498644246145:web:124baa39217b2693007780"
+    apiKey: "AIzaSyDK-cSKaQkfsqQ9w7ggC-b82dwLX0h30yA",
+    authDomain: "fir-authentication-3bccd.firebaseapp.com",
+    projectId: "fir-authentication-3bccd",
+    storageBucket: "fir-authentication-3bccd.appspot.com",
+    messagingSenderId: "643924871001",
+    appId: "1:643924871001:web:1c6908af22ed0b3ad26e48"
 };
+
 
 export default firebaseConfig;
